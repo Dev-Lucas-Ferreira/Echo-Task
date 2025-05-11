a0d422dacb297a1bbf5c103ee50f722596dfa2e2
Lucas Gabriel <163341948+Dev-Lucas-Ferreira@users.noreply.github.com>
2025-05-11 23:08:50 +0000

    Alterado para poder ser acessado com link

---
+++ Echo-Task/public/js/index.js
@@ -0,0 +1,7 @@
+function paginas(x) {
+    if (x == 1) {
+        window.location.href = "login.html";
+    } else if (x == 2) {
+        window.location.href = "cadastro.html";
+    }
+}
--- Echo-Task1/public/js/index.js
+++
@@ -1,7 +0,0 @@
-function paginas(x) {
-    if (x == 1) {
-        window.location.href = "../public/login.html";
-    } else if (x == 2) {
-        window.location.href = "../public/cadastro.html";
-    }
-}
